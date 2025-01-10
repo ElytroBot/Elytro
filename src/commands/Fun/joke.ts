0a8aebbdb4fde68e64caf8728f2dcdaff154cd30
eliasciur <afakedemail@yahoo.com>
2025-01-10 10:36:58 -0600

    updated to use jokeapi.dev

--- src/commands/Fun/joke.ts
+++ src/commands/Fun/joke.ts
@@ -1,7 +1,6 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../structure/Command';
 import { EmbedColor } from '../../structure/EmbedColor';
-import jokes from '../../json/jokes.json';
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,14 +8,30 @@ module.exports = {
 		.setDescription('Tells you a joke.'),
 
 	async onCommandInteraction(interaction) {
-		interaction.reply({
-			embeds: [
-				new EmbedBuilder({
-					color: EmbedColor.primary,
-					title: 'Joke',
-					description: jokes[Math.floor(Math.random() * jokes.length)]
-				})
-			]
-		});
+		fetch('https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
+			.then(res => res.json())
+			.then(json => {
+				interaction.reply({
+					embeds: [
+						new EmbedBuilder({
+							color: EmbedColor.primary,
+							title: 'Joke',
+							description: json.type == 'single'? json.joke : `${json.setup}\n${json.delivery}`,
+							footer: { text: 'Powered by jokeapi.dev' }
+						})
+					]
+				});
+			})
+			.catch(() => {
+				interaction.reply({
+					embeds: [
+						new EmbedBuilder({
+							color: EmbedColor.danger,
+							description: 'It looks like there was an issue with our joke API. Please try again later.'
+						})
+					],
+					ephemeral: true
+				});
+			});
 	}
 } satisfies Command;
