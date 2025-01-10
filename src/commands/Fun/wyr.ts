7185580f7d957ecb88f093eef09d434a53e9ad51
eliasciur <afakedemail@yahoo.com>
2025-01-10 10:38:05 -0600

    updated to use wyr API from rapidapi.com

--- src/commands/Fun/wyr.ts
+++ src/commands/Fun/wyr.ts
@@ -1,7 +1,6 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../structure/Command';
 import { EmbedColor } from '../../structure/EmbedColor';
-import wyr from '../../json/wyr.json';
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,14 +8,33 @@ module.exports = {
 		.setDescription('Asks a would you rather question.'),
 
 	async onCommandInteraction(interaction) {
-		interaction.reply({
-			embeds: [
-				new EmbedBuilder({
-					color: EmbedColor.primary,
-					title: 'Would You Rather',
-					description: wyr[Math.floor(Math.random() * wyr.length)]
-				})
-			]
-		});
+		fetch(
+			'https://would-you-rather.p.rapidapi.com/wyr/random',
+			{ headers: { 'x-rapidapi-key': process.env.RAPID_API_KEY } }
+		)
+			.then(res => res.json())
+			.then(json => {
+				interaction.reply({
+					embeds: [
+						new EmbedBuilder({
+							color: EmbedColor.primary,
+							title: 'Would You Rather',
+							description: json[0].question,
+							footer: { text: 'Powered by rapidapi.com' }
+						})
+					]
+				});
+			})
+			.catch(() => {
+				interaction.reply({
+					embeds: [
+						new EmbedBuilder({
+							color: EmbedColor.danger,
+							description: 'It looks like there was an issue with our wyr API. Please try again later.'
+						})
+					],
+					ephemeral: true
+				});
+			});
 	}
 } satisfies Command;
